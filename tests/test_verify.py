05900de4b89d839d1257f646c85b8a0f6b0f72dc
Elias <elias.bonnici@yubico.com>
2023-10-24 17:07:57 +0200

    Add fixtures to preserve different repository states.

--- tests/test_verify.py
+++ tests/test_verify.py
@@ -12,42 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from gitbark.core import BARK_RULES_BRANCH
-from gitbark.objects import BarkRules, BranchRule
-from gitbark.util import cmd
-
 from .util import (
-    get_test_bark_module,
-    disable_bark,
     Environment,
     Repo,
 )
 
 from typing import Optional, Callable
-from unittest.mock import patch
 
 import pytest
-import os
 
-
-@pytest.fixture(autouse=True, scope="module")
-def initialize_bark(env: Environment, bark_cli):
-    # create the first commit
-    bootstrap_main = env.repo.commit("Initial commit.")
-
-    # create bark_rules
-    branch_rule = BranchRule(
-        pattern="main", bootstrap=bootstrap_main.hash, ff_only=False
-    )
-    bark_rules = BarkRules(branches=[branch_rule], modules=[get_test_bark_module()])
-    env.repo.add_bark_rules(bark_rules)
-
-    cwd = os.getcwd()
-    os.chdir(env.repo.repo_dir)
-    # bark install
-    with patch("click.confirm", return_value="y"):
-        bark_cli("install")
-    os.chdir(cwd)
 
 class TestVerify:
     def verify_rules(
@@ -58,12 +31,6 @@ class TestVerify:
         commit_rules: Optional[dict] = None,
         bark_rules: Optional[dict] = None,
     ):
-
-        curr_branch_head_a = env.repo.head
-        curr_bark_rules_head = cmd(
-            "git", "rev-parse", "bark_rules", cwd=env.repo.repo_dir
-        )[0]
-
         if commit_rules:
             env.repo.add_commit_rules(commit_rules)
 
@@ -71,11 +38,6 @@ class TestVerify:
             env.repo.add_bark_rules(bark_rules)
 
         self.verify_action(env, passes, action)
-
-        # Reset to previous state
-        with disable_bark(env.repo) as repo:
-            repo.revert(curr_branch_head_a)
-            repo.revert(curr_bark_rules_head, BARK_RULES_BRANCH)
 
     def verify_action(
         self, env: Environment, passes: bool, action: Callable[[Repo], None]
@@ -123,10 +85,10 @@ class TestVerify:
             },
         ],
     )
-    def test_commit_rule_validation(self, env: Environment, rules):
+    def test_commit_rule_validation(self, env_installed: Environment, rules):
         action: Callable[[Repo], None] = lambda repo: repo.commit()
         self.verify_rules(
-            env=env,
+            env=env_installed,
             passes=rules["passes"],
             action=action,
             commit_rules=rules["cr_rules"],
@@ -150,6 +112,10 @@ class TestVerify:
             },
         ],
     )
-    def test_add_broken_commit_rules(self, env: Environment, rules):
+    def test_add_broken_commit_rules(self, env_installed: Environment, rules):
         action: Callable[[Repo], None] = lambda repo: repo.add_commit_rules(rules)
-        self.verify_rules(env=env, passes=False, action=action)
+        self.verify_rules(env=env_installed, passes=False, action=action)
+
+    def test_commit_with_invalid_bark_rules(self, env_bark_rules_invalid: Environment):
+        action: Callable[[Repo], None] = lambda repo: repo.commit()
+        self.verify_rules(env=env_bark_rules_invalid, passes=False, action=action)
